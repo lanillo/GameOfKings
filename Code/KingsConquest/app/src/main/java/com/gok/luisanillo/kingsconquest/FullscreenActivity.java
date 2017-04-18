714153cbbc4028fe45485e1e46e5e8984cb4ab85
lanillo <luis.felipe.anillo@usherbrooke.ca>
2017-04-17 23:39:56 -0400

    Erreur
    Probalmeent dans le .java avec les nouveaux ID

--- Code/KingsConquest/app/src/main/java/com/gok/luisanillo/kingsconquest/FullscreenActivity.java
+++ Code/KingsConquest/app/src/main/java/com/gok/luisanillo/kingsconquest/FullscreenActivity.java
@@ -90,8 +90,8 @@ public class FullscreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fullscreen);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        mControlsView = findViewById(R.id.toplayer);
+        mContentView = findViewById(R.id.buttons);
 
         // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
@@ -104,7 +104,7 @@ public class FullscreenActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.yes).setOnTouchListener(mDelayHideTouchListener);
+        //findViewById(R.id.yes).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
