b4dd610426951311dec1619b3701836ff69a5cb9
lanillo <luis.felipe.anillo@usherbrooke.ca>
2017-04-18 23:17:15 -0400

    Nouvelle Activite --  Help --Compile pas
    Ajout d'une nouvelle activity

---
+++ Code/KingsConquest/app/src/main/java/com/gok/luisanillo/kingsconquest/HelpMenu.java
@@ -0,0 +1,23 @@
+package com.gok.luisanillo.kingsconquest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelpMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_menu);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.helptext);
+        textView.setText(message);
+    }
+}
--- Code/KingsConquest/app/src/main/java/com/gok/luisanillo/kingsconquest/MainActivity.java
+++ Code/KingsConquest/app/src/main/java/com/gok/luisanillo/kingsconquest/MainActivity.java
@@ -1,13 +1,55 @@
 package com.gok.luisanillo.kingsconquest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Constants */
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the @string/NewGame Button */
+    public void newGame(View view) {
+        // Start a new game
+
+    }
+
+    /** Called when the user taps the @string/loadGame Button */
+    public void loadGame(View view) {
+        // Load a previous game
+
+    }
+
+    /** Called when the user taps the @string/help Button */
+    public void help(View view) {
+        // Show help menu
+        Intent intent = new Intent(this, HelpMenu.class);
+        EditText editText = (EditText) findViewById(R.id.helptext);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    /** Called when the user taps the @string/credits Button */
+    public void credits(View view) {
+        // Show credits
+
+    }
+
+    /** Called when the user taps the @string/quit Button */
+    public void quit(View view) {
+        // Quit the application
+
     }
 }
