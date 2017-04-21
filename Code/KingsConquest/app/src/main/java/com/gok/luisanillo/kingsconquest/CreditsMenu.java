bb333281faafb4bfdbf39dc1de3de63bdc50b311
lanillo <luis.felipe.anillo@usherbrooke.ca>
2017-04-20 22:16:22 -0400

    Back buttons and optimization (#2)
    Added Back buttons and implemented them, optimized app by closing
    actiivties and commented code

--- Code/KingsConquest/app/src/main/java/com/gok/luisanillo/kingsconquest/CreditsMenu.java
+++ Code/KingsConquest/app/src/main/java/com/gok/luisanillo/kingsconquest/CreditsMenu.java
@@ -2,12 +2,32 @@ package com.gok.luisanillo.kingsconquest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 
 public class CreditsMenu extends AppCompatActivity {
 
+    /** Called when Credits on MainActivity is pressed *
+     * Function : Creates the activity */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits_menu);
     }
+
+    /** Called when back button is pressed
+     * Function : Closes the activity */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+
+    /** Called when the back arrow is pressed
+     * Function : Returns to main menu and closes activity */
+    public void back(View view) {
+        this.finish();
+    }
+
+
 }
