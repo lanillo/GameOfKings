3f8cf97ffe2c1aeb3f521a24c637582ad9e2462d
lanillo <luis.felipe.anillo@usherbrooke.ca>
2017-05-01 01:12:26 -0400

    Created new activity and forced app to be horizontal

---
+++ Code/KingsConquest/app/src/main/java/com/gok/luisanillo/kingsconquest/GameStart.java
@@ -0,0 +1,31 @@
+package com.gok.luisanillo.kingsconquest;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class GameStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_start);
+
+    }
+
+}
